27834732c4478f9d41c5c820c2d17584eab86d77
Etoilaria <105894701+Etoilaria@users.noreply.github.com>
2024-06-28 14:50:53 -0400

    User Input Function
    When gridsize button is clicked, the website displays a pop-up asking for a number from 1-100. If it is not a number between 1-100, it displays a error message.

--- Etch-a-Sketch/EAS.js
+++ Etch-a-Sketch/EAS.js
@@ -1,3 +1,21 @@
+//Function for user input on grid size
+function Gridsize() {
+    var size = prompt("Choose a number from 1-100:")
+    var isNum = !isNaN(Number(size))
+    var isBoundary = size >= 1 && size <= 100
+    if (isNum) {
+        if (isBoundary) {
+            alert("Changing grid size...")
+        }
+        else {
+            alert("You didn't enter a number from 1-100")
+        }
+    }
+    else {
+        alert("You didn't enter a valid number")
+    }
+}
+// Function to create grid //
 document.addEventListener("DOMContentLoaded", function() {
     const container = document.getElementById("main-container");
 
